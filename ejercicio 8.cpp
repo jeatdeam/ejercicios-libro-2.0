2c03a7d389b36bba660a4a561702bd453c5263ec
jeatdeam <48858548+jeatdeam@users.noreply.github.com>
2019-04-01 20:24:39 -0500

    Add files via upload

---
+++ ejercicio 8.cpp
@@ -0,0 +1,10 @@
+
+/*a) Los comentarios se utilizan para documentar un programa y mejorar su calidad
+
+b)El objeto que se utilizar para imprimir información en la pantalla es COUT
+
+c)Una instrucción en c++ que toma una decisión es la instrucción IF
+
+d) La mayoría de los cálculos se realizan normalmente mediante instrucciones de declarar variables
+
+e) El objeto CIN recibe valores de entrada del teclado*/
---
+++ ejercicio 9.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main() {
+	/* a)  cout << "escriba 2 numeros";    */
+	/* b) #include<iostream>
+          using namespace std;
+          int main() {
+	      int a, b, producto = 0;
+	      cin >> a;cin >> b;
+	      producto = a * b;
+	      cout << "el valor de c es :" << producto<< endl;
+	      system("pause");
+	      return 0;
+          }
+       d)int a,b,c;
+	    cin>>a;cin>>b;cin>>c;                   */
+
+
+	system("pause");
+	return 0;
+}
